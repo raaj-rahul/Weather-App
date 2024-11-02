2552feb6953d175afa929329d037755f775c3ada
raaj-rahul <rahul1492002@gmail.com>
2024-11-02 08:55:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
+
+$(document).ready(function () {
+    // Load default weather for Patna
+    weatherFn('Patna');
+});
+
+async function weatherFn(cName) {
+    const temp = `${url}?q=${cName}&appid=${apiKey}&units=metric`;
+    try {
+        const res = await fetch(temp);
+        const data = await res.json();
+        if (res.ok) {
+            weatherShowFn(data);
+        } else {
+            alert('City not found. Please try again.');
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+function weatherShowFn(data) {
+    $('#city-name').text(data.name);
+    $('#date').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    $('#temperature').html(`${data.main.temp}°C`);
+    $('#description').text(data.weather[0].description);
+    $('#wind-speed').html(`Wind Speed: ${data.wind.speed} m/s`);
+    
+    // Set the weather icon URL based on the API response
+    const iconCode = data.weather[0].icon; // Get the icon code from the API response
+    $('#weather-icon').attr('src', `https://openweathermap.org/img/wn/${iconCode}@2x.png`);
+    
+    $('#weather-info').fadeIn();
+}
